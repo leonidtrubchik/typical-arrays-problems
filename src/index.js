ee05e1e4e74dfde9a1f6bcad32b02afcc255b7ba
leonid trubchik <leon.d.e@mail.ru>
2020-10-03 23:43:41 +0300

    init: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length > 0) {
+    let maxValue = Math.min.apply(null, array)
+    return maxValue
+    
+  }
+  else {
+    return 0
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length > 0) {
+    let minValue = Math.max.apply(null, array)
+    return minValue
+    
+  }
+  else {
+    return 0
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) && array.length > 0) {
+
+    let sum = 0
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i]
+    }
+    return sum / array.length
+    
+  }
+  else {
+    return 0
+  }
 }
